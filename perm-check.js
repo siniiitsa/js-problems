dfb3d88ef67cb736a2a6b8081720573f6b3a454f
siniiitsa <siniiitsa@gmail.com>
2019-11-16 13:02:27 +0200

    perm-check problem complete

---
+++ perm-check.js
@@ -0,0 +1,65 @@
+/*
+A non-empty array A consisting of N integers is given.
+
+A permutation is a sequence containing each element from 1 to N once, and only once.
+
+For example, array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+
+is a permutation, but array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+
+is not a permutation, because value 2 is missing.
+
+The goal is to check whether array A is a permutation.
+
+Write a function:
+
+function solution(A);
+
+that, given an array A, returns 1 if array A is a permutation and 0 if it is not.
+
+For example, given array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+
+the function should return 1.
+
+Given array A such that:
+
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+
+the function should return 0.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [1..1,000,000,000].
+*/
+
+function solution(A) {
+  const a = [...A].sort();
+
+  if (a[0] !== 1) return 0;
+
+  for (let i = 1; i < a.length; ++i) {
+    if (a[i] !== a[i - 1] + 1) return 0;
+  }
+
+  return 1;
+}
+
+const numbers = [4, 1, 3, 2];
+console.log(solution(numbers));
